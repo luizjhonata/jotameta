a43ed025eaea21e4c001f9f8ae69c2c7070b62f7
Jhonata Luiz <jhonataluiz@hotmail.com>
2022-09-09 12:51:22 -0300

    Project Created

---
+++ backend/src/main/java/com/luizjhonata/jotameta/JotametaApplication.java
@@ -0,0 +1,13 @@
+package com.luizjhonata.jotameta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JotametaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JotametaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/luizjhonata/jotameta/JotametaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizjhonata.jotameta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JotametaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
