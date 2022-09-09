314ec6d7606d2b2cb6aa7f391a13c02b271fd8f2
Jhonata Luiz <jhonataluiz@hotmail.com>
2022-09-09 18:39:46 -0300

    Twilio  SMS

--- backend/src/main/java/com/luizjhonata/jotameta/controllers/SaleController.java
+++ backend/src/main/java/com/luizjhonata/jotameta/controllers/SaleController.java
@@ -2,13 +2,11 @@ package com.luizjhonata.jotameta.controllers;
 
 import com.luizjhonata.jotameta.entities.Sale;
 import com.luizjhonata.jotameta.services.SaleService;
+import com.luizjhonata.jotameta.services.SmsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,6 +16,8 @@ public class SaleController {
 
     @Autowired
     private SaleService service;
+    @Autowired
+    private SmsService smsService;
 
     @GetMapping
     public Page<Sale> findSales(
@@ -25,5 +25,10 @@ public class SaleController {
             @RequestParam(value="maxDate", defaultValue = "") String maxDate,
             Pageable pageable){
         return service.findSales(minDate, maxDate, pageable);
+    }
+
+    @GetMapping("/{id}/notification")
+    public void notifySms(@PathVariable Long id) {
+        smsService.sendSms(id);
     }
 }
